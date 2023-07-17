09cef5903960f56e18a875dba1ac20c2a0d97104
Pavel Zelenka <pavel.zelenka@principal.cz>
2023-07-17 15:34:53 +0200

    Základ logging - not finished!

---
+++ Principal.Telemedicine/Principal.Telemedicine.Shared.Logging/CustomLoggerExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Principal.Telemedicine.Shared.Logging.Enumerators;
+using System;
+
+namespace Principal.Telemedicine.Shared.Logging;
+public static class CustomLoggerExtensions
+{
+    public static void LogCustom<T>(this ILogger<T> logger, CustomLogLevel customLogLevel, string friendlyTopic, string source, string shortMessage,  string fullMessage, string aditionalInfo )
+    {
+        LogLevel logLevel = MapCustomLogLevelToLogLevel(customLogLevel);
+        string message = $"{friendlyTopic.Replace("|", "-")}|{source.Replace("|","-")}|{shortMessage.Replace("|", "-")}|{fullMessage.Replace("|", "-")}|{aditionalInfo.Replace("|", "-")}";
+        logger.Log(logLevel, message);
+    }
+
+    private static LogLevel MapCustomLogLevelToLogLevel(CustomLogLevel customLogLevel)
+    {
+        switch (customLogLevel)
+        {
+            case CustomLogLevel.Information:
+                return LogLevel.Information;
+            case CustomLogLevel.Warning:
+                return LogLevel.Warning;
+            case CustomLogLevel.Error:
+                return LogLevel.Error;
+            case CustomLogLevel.Audit:
+                return LogLevel.None; 
+            default:
+                throw new ArgumentException($"Unknown custom log level: {customLogLevel}", nameof(customLogLevel));
+        }
+    }
+}
---
+++ Principal.Telemedicine/Principal.Telemedicine.Shared.Logging/TelemedicineDbLogger.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Principal.Telemedicine.DataConnectors.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principal.Telemedicine.Shared.Logging;
+public class TelemedicineDbLogger:ILogger
+{
+    private readonly string _categoryName;
+    private readonly DbContextGeneral _context;
+
+    public TelemedicineDbLogger(string categoryName, DbContextGeneral context)
+    {
+        _categoryName = categoryName;
+       _context = context;
+    }
+
+    public IDisposable BeginScope<TState>(TState state)
+    {
+        return null;
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+      
+        if (logLevel == LogLevel.None)
+        {
+            return true;
+        }
+
+
+        return logLevel >= LogLevel.Information;
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+    {
+        if (!IsEnabled(logLevel))
+        {
+            return;
+        }
+
+        var message = formatter(state, exception);
+
+        if (string.IsNullOrEmpty(message) && exception == null)
+        {
+            return;
+        }
+
+        if (exception != null)
+        {
+            var logEntry = new Log
+            {
+              //...
+                CreatedDateUtc = DateTime.UtcNow
+            };
+
+            // Uložení logovacího záznamu do databáze
+            _context.Logs.Add(logEntry);
+
+        }
+        else
+        {
+            var logEntry = new Log()
+            {
+             //.....
+                CreatedDateUtc = DateTime.UtcNow
+            };
+
+            // Uložení logovacího záznamu do databáze
+            _context.Add(logEntry);
+        }
+        _context.SaveChanges();
+    }
+}
+
