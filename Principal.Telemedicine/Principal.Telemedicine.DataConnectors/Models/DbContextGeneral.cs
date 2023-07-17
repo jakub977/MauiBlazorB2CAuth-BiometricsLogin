dc38cbf6494c2e8e8d37675d39655c4c990d3776
Pavel Zelenka <pavel.zelenka@principal.cz>
2023-07-17 16:20:12 +0200

    gitignore

---
+++ Principal.Telemedicine/Principal.Telemedicine.DataConnectors/Models/DbContextGeneral.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Principal.Telemedicine.DataConnectors.Models;
+
+public partial class DbContextGeneral : DbContext
+{
+    private readonly string _connectionString;
+
+    public DbContextGeneral()
+    {
+    }
+
+    public DbContextGeneral(DbContextOptions<DbContextGeneral> options)
+        : base(options)
+    {
+    }
+
+    public DbContextGeneral(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public virtual DbSet<Log> Logs { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured && !string.IsNullOrEmpty(_connectionString))
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.UseCollation("SQL_Latin1_General_CP1250_CI_AS");
+
+        modelBuilder.Entity<Log>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tmp_ms_x__3214EC0771B54912");
+
+            entity.Property(e => e.CreatedDateUtc).HasDefaultValueSql("(getutcdate())");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
