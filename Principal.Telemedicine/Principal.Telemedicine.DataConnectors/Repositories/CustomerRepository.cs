75b20522fffc62fa3f1c6f537ff60e89e66290a7
Lenka Camprová <lenka.camprova@principal.cz>
2023-10-05 11:28:34 +0200

    SaveUpdateToken api

--- Principal.Telemedicine/Principal.Telemedicine.DataConnectors/Repositories/CustomerRepository.cs
+++ Principal.Telemedicine/Principal.Telemedicine.DataConnectors/Repositories/CustomerRepository.cs
@@ -6,34 +6,35 @@ namespace Principal.Telemedicine.DataConnectors.Repositories;
 
 /// <inheritdoc/>
 public class CustomerRepository : ICustomerRepository
+{
+
+    private readonly DbContextApi _dbContext;
+
+    public CustomerRepository(DbContextApi dbContext)
     {
+        _dbContext = dbContext;
+    }
 
-        private readonly DbContextApi _dbContext;
+    /// <inheritdoc/>
+    public async Task<IEnumerable<Customer>> GetCustomersTaskAsyncTask()
+    {
+        var listOfCustomers = await _dbContext.Customers.OrderBy(p => p.Id).ToListAsync();
 
-        public CustomerRepository(DbContextApi dbContext)
-        {
-            _dbContext = dbContext;
-        }
+        return listOfCustomers;
+    }
 
-        /// <inheritdoc/>
-        public async Task<IEnumerable<Customer>> GetCustomersTaskAsyncTask()
-        {
-            var listOfCustomers = await _dbContext.Customers.OrderBy(p => p.Id).ToListAsync();
+    /// <inheritdoc/>
+    public async Task<Customer?> GetCustomerByIdTaskAsync(int id)
+    {
+        var customer = await _dbContext.Customers
+            .Include(p => p.EffectiveUserUsers).ThenInclude(efus => efus.RoleMembers).DefaultIfEmpty()// efektivního uživatele mají jenom uživatelé, kteřé mají vyplněné ProviderId - do RoleMember vazba přes EffectiveUserId -- pacient, lékař atd.
+            .Include (p => p.RoleMemberDirectUsers).DefaultIfEmpty() // uživatelé bez ProviderId mají vazbu do RoleMember přes DirectUserId -- administrativní role
+            .Include( p => p.UserPermissionUsers).DefaultIfEmpty() //DeniedPermissions
+            .Where(p => p.Id == id).FirstOrDefaultAsync();
 
-            return listOfCustomers;
-        }
+        return customer;
+    }
 
-        /// <inheritdoc/>
-        public async Task<Customer?> GetCustomerByIdTaskAsync(int id)
-        {
-             var customer = await _dbContext.Customers
-                 .Include(p => p.EffectiveUserUsers).ThenInclude(efus => efus.RoleMembers).DefaultIfEmpty()// efektivního uživatele mají jenom uživatelé, kteřé mají vyplněné ProviderId - do RoleMember vazba přes EffectiveUserId -- pacient, lékař atd.
-                 .Include (p => p.RoleMemberDirectUsers).DefaultIfEmpty() // uživatelé bez ProviderId mají vazbu do RoleMember přes DirectUserId -- administrativní role
-                 .Include( p => p.UserPermissionUsers).DefaultIfEmpty() //DeniedPermissions
-                 .Where(p => p.Id == id).FirstOrDefaultAsync();
-
-             return customer;
-        }
     /// <inheritdoc/>
     public async Task<Customer?> GetCustomerByGlobalIdTaskAsync(string globalId)
     {
