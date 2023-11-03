868bf9243da62c4094f0b57c1cb7329ed91fdd20
Lenka Camprová <lenka.camprova@principal.cz>
2023-11-03 12:31:28 +0100

    Generické odpovědi a úprava návratových dat

--- Principal.Telemedicine/Principal.Telemedicine.PenelopeData/Models/ScheduledActivitiesDataModel.cs
+++ Principal.Telemedicine/Principal.Telemedicine.PenelopeData/Models/ScheduledActivitiesDataModel.cs
@@ -1,4 +1,6 @@
-﻿namespace Principal.Telemedicine.PenelopeData.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace Principal.Telemedicine.PenelopeData.Models;
 
 /// <summary>
 /// Data model of patients scheduled activities - specific for Penelope.
@@ -8,40 +10,48 @@ public class ScheduledActivitiesDataModel
     /// <summary>
     /// Unique identifier of activity.
     /// </summary>
+    [JsonPropertyName("ActivityUniqueId")]
     public Guid ActivityUniqueId { get; set; }
 
     /// <summary>
     /// Date time of scheduled activity, using coordinated universal time
     /// </summary>
+    [JsonPropertyName("ActivityDate")]
     public DateTime ActivityDate { get; set; }
 
     /// <summary>
     /// Type of scheduled activity
     /// </summary>
+    [JsonPropertyName("ActivityType")]
     public string? ActivityType { get; set; }
 
     /// <summary>
     /// Code of activity
     /// </summary>
+    [JsonPropertyName("ActivityCode")]
     public string? ActivityCode { get; set; }
 
     /// <summary>
     /// Body of notifitication related to the activity
     /// </summary>
+    [JsonPropertyName("Notification")]
     public string? Notification { get; set; }
 
     /// <summary>
     /// Icon of activity
     /// </summary>
+    [JsonPropertyName("Icon")]
     public string? Icon { get; set; }
 
     /// <summary>
     /// Bit identifier if scheduled activity is completed
     /// </summary>
+    [JsonPropertyName("IsCompleted")]
     public int? IsCompleted { get; set; }
 
     /// <summary>
     /// Date time of scheduled activity update, using coordinated universal time
     /// </summary>
+    [JsonPropertyName("LastUpdatedDateUtc")]
     public DateTime LastUpdatedDateUtc { get; set; }
 }
